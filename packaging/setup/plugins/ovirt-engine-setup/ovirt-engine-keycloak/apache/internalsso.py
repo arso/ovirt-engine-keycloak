97722b8a34ab2e4845fc8708330dfa9a1d1f53fe
Artur Socha <asocha@redhat.com>
2022-02-02 12:19:33 +0100

    Setup for internal sso(openidc)
    This patch prepares Keycloak realm and client for ovirt engine internal
    sso needs.
    This includes:
    - creating dedicated Keycloak realm, client and necessary scopes.
    - creating Keycloak internal admin user that mimics already existing admin@internal
    - preparing ovirt engine extension configuration files
    - preparing httpd openidc configuration
    
    Very important assumption is that keycloak based internal sso is enabled
    only for *new* and *RPM* based deployment of ovirt-engine. That means that
    authentication for development mode stays as it is as well as for
    upgraded installations.
    
    Future steps (outside of the scope of this patch) includes:
    - replacing jdbc based internal sso for non-dev environments
    - preparing standalone tool for jdbc users migration for upgrading
      existing installation.
    
    Signed-off-by: Artur Socha <asocha@redhat.com>

---
+++ packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-keycloak/apache/internalsso.py
@@ -0,0 +1,90 @@
+#
+# ovirt-engine-setup -- ovirt engine setup
+#
+# Copyright oVirt Authors
+# SPDX-License-Identifier: Apache-2.0
+#
+#
+
+"""keycloak httpd internal sso openidc config plugin."""
+
+import gettext
+import secrets
+
+from otopi import constants as otopicons
+from otopi import filetransaction
+from otopi import plugin
+from otopi import util
+from ovirt_engine import util as outil
+from ovirt_engine_setup import constants as osetupcons
+from ovirt_engine_setup.engine import constants as oenginecons
+from ovirt_engine_setup.engine_common import constants as oengcommcons
+from ovirt_engine_setup.keycloak import constants as okkcons
+
+
+def _(m):
+    return gettext.dgettext(message=m, domain='ovirt-engine-keycloak')
+
+
+@util.export
+class Plugin(plugin.PluginBase):
+    """keycloak httpd internal sso openidc config plugin."""
+
+    def __init__(self, context):
+        super(Plugin, self).__init__(context=context)
+
+    @plugin.event(
+        stage=plugin.Stages.STAGE_INIT,
+    )
+    def _init(self):
+        self.environment.setdefault(
+            okkcons.ApacheEnv.HTTPD_CONF_OVIRT_ENGINE_INTERNAL_SSO_OPENIDC,
+            okkcons.FileLocations.HTTPD_CONF_OVIRT_ENGINE_INTERNAL_SSO_OPENIDC,
+        )
+
+    @plugin.event(
+        stage=plugin.Stages.STAGE_MISC,
+        condition=lambda self: (
+                self.environment[oenginecons.CoreEnv.ENABLE] and
+                not self.environment[osetupcons.CoreEnv.DEVELOPER_MODE]
+        ),
+        after=(
+            okkcons.Stages.CLIENT_SECRET_GENERATED,
+        )
+    )
+    def _internalsso_openidc(self):
+        self.environment[oengcommcons.ApacheEnv.NEED_RESTART] = True
+        self.environment[otopicons.CoreEnv.MAIN_TRANSACTION].append(
+            filetransaction.FileTransaction(
+                name=self.environment[
+                    okkcons.ApacheEnv.HTTPD_CONF_OVIRT_ENGINE_INTERNAL_SSO_OPENIDC
+                ],
+                content=outil.processTemplate(
+                    template=(
+                        okkcons.FileLocations.HTTPD_CONF_OVIRT_ENGINE_INTERNAL_SSO_OPENIDC_TEMPLATE
+                    ),
+                    subst={
+                        '@CLIENT_SECRET@': self.environment[
+                            okkcons.ConfigEnv.KEYCLOAK_OVIRT_INTERNAL_CLIENT_SECRET
+                        ],
+                        '@CLIENT_ID@':
+                            okkcons.Const.KEYCLOAK_INTERNAL_CLIENT_NAME,
+                        '@OVIRT_REALM@':
+                            okkcons.Const.KEYCLOAK_INTERNAL_REALM,
+                        '@ENGINE_FQDN@': self.environment[
+                            oenginecons.ConfigEnv.ENGINE_FQDN
+                        ],
+                        '@KEYCLOAK_WEB_CONTEXT@':
+                            okkcons.Const.KEYCLOAK_WEB_CONTEXT,
+                        '@RANDOM_CRYPTO_PASSPHRASE@':
+                            secrets.token_urlsafe(nbytes=16),
+                    },
+                ),
+                modifiedList=self.environment[
+                    otopicons.CoreEnv.MODIFIED_FILES
+                ],
+            )
+        )
+
+
+# vim: expandtab tabstop=4 shiftwidth=4
